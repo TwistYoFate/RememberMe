b165e191f8f89cd8e02c8e57f749cead45bc9755
Deepanshu Yadav <36413670+TwistYoFate@users.noreply.github.com>
2020-11-07 16:25:51 +0530

    Added Sprites for background and Tile

---
+++ RememberMe/Assets/Scripts/Store.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+    #region Singleton
+    private static Store _store = null;
+    public static Store store
+    {
+        get
+        {
+            return _store;
+        }
+    }
+    private void Awake()
+    {
+        if ((_store != null) || (_store != this))
+        {
+            Destroy(_store);
+        }
+        _store = this;
+    }
+    #endregion
+
+    //Mutexes
+
+    private int _tile_dimension = 0;
+
+    //Properties
+    public int tile_dimension
+    {
+        get
+        {
+            return _tile_dimension;
+        }
+
+        set
+        {
+            _tile_dimension = value; 
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
